32c7a68fac483683105c2e1a29f969128e6b0680
Tobiah Ens <brickmantoby@gmail.com>
2023-08-16 12:49:04 -0700

    Physics thing

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,8 +1,31 @@
+let x = 50
+let xV = 0;
+let y = 500
+let yV = 0;
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  
 }
 
 function draw() {
   background(220);
-  circle(mouseX, mouseY, 20);
+  circle(x, y, 50);
+  x += xV
+  xV *= 0.97
+  y += yV
+  if (y < 25){
+    y = 25
+    yV = -yV *0.5
+  } else {
+    yV -= 0.7
+  }
+}
+
+function mousePressed(){
+  yV = 15
+  xV = (mouseX - x)/abs(mouseX - x)*10
+}
+
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight)
 }
